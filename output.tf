c875c1415249a28c5d4e0c8b168c9428b81196db
Ram <rgopinat@redhat.com>
2025-01-31 21:48:00 -0800

    outputing db instance endpoints

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "alb_dns_name" {
   description = "The DNS name of the load balancer"
   value       = module.alb.dns_name
 }
+output "rds_endpoints" {
+  description = "RDS database endpoints"
+  value = { for k, db in module.rds : k => db.db_instance_endpoint}
+}
